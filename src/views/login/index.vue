b987de4510998cd2d7f2df8e9a073d9712adb05e
unknown <chencliu@yeah.net>
2020-12-11 23:08:00 +0800

    2020-12-11

---
+++ src/views/login/index.vue
@@ -0,0 +1,26 @@
+<!--  -->
+<template>
+  <div class="login-container">登录界面</div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex',
+
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
